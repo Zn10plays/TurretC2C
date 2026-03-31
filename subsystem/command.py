474d98d18b93ce5f8722461bf653bdca7acb5113
Chase C. <rudrapatel26122004@gmail.com>
2026-03-31 10:30:48 -0400

    stash

--- subsystem/command.py
+++ subsystem/command.py
@@ -1,12 +1,14 @@
 from subsystem.structure import Subsystem
 from enum import Enum
+import asyncio
+from models.MotorStatus import MotorPositionLog
 import time
 
 class ControlMode(Enum):
-    Survey: 1
-    Idle: 2
-    LockOn: 3
-    TeleOp: 4
+    Idle: 1
+    Survey: 2
+    TeleOp: 3
+    LockOn: 4
 
 
 class CommandSubsystem(Subsystem):
@@ -15,11 +17,38 @@ class CommandSubsystem(Subsystem):
 
         # start of doing nothing
         self.current_state = ControlMode.Idle
-    
-    def start(self):
-        return super().start()
-    
-
-    
-
         
+        self.bus.subscribe('ChangeControlState', self.update_control_state)
+        self.bus.subscribe('MotorLogs')
+
+        # init 90 deg pitch, no yaw, no velocity
+        self.current_turret_pos = MotorPositionLog(time.monotonic_ns(), [0.25, 0], [0, 0])
+    
+    def update_control_state(self, new_state: ControlMode):
+        self.current_state = new_state
+        pass
+
+    def update_current_motor_setpoints(self, motor_logs: MotorPositionLog):
+        self.current_turret_pos = motor_logs
+        pass
+
+    async def start(self):
+        while True:
+            match self.current_state:
+                case ControlMode.Idle:
+                    # do nothing
+                    pass
+                case ControlMode.Survey:
+                    # scan surrondings
+                    pass
+                case ControlMode.TeleOp:
+                    # go to user command, or do nothing
+                    # fire is always a user command
+                    pass
+                case ControlMode.LockOn:
+                    # follow target, cascade pid with moteus
+                    # issue is, we need to fid the target
+                    pass
+            
+            await asyncio.sleep(0.01)
+                
---
+++ utility/paths.py
@@ -0,0 +1,9 @@
+from models.MotorStatus import MotorPositionLog
+from models.MoveCommand import MoveCommand
+
+# defines precomputed paths for the turret to follow.
+
+class SurvayPath():
+    def get_next_setpoint(current_orentation: MotorPositionLog) -> MoveCommand:
+        
+        pass
